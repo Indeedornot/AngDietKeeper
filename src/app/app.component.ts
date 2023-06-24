588beda6d30f095fb5be35303ef0e617aba5635e
Indeedornot <gravitybartek@gmail.com>
2023-06-24 10:55:41 +0200

    add data binding and update button style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
 	title = 'AngDietKeeper';
 	cardAmount = 12;
+	budget = 100;
+	budgetLeft = this.budget;
+	currencySymbol = '$';
+
+	calories = 2000;
+	caloriesLeft = this.calories;
 }
