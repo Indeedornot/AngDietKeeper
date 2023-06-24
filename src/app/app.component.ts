5867750abda3dfe57b2cd43cd324291c048e31f8
Indeedornot <gravitybartek@gmail.com>
2023-06-24 11:17:07 +0200

    use spaces not tabs to fit with angular styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	title = 'AngDietKeeper';
-	cardAmount = 12;
-	budget = 100;
-	budgetLeft = this.budget;
-	currencySymbol = '$';
+  title = 'AngDietKeeper';
+  cardAmount = 12;
+  budget = 100;
+  budgetLeft = this.budget;
+  currencySymbol = '$';
 
-	calories = 2000;
-	caloriesLeft = this.calories;
+  calories = 2000;
+  caloriesLeft = this.calories;
 }
